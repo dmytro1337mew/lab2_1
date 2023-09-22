ed64196130a52c72e8cb81a2f9196e23146f8e6d
dmytro1337mew <105820092+dmytro1337mew@users.noreply.github.com>
2023-09-22 11:18:53 +0300

    Initial Commit

---
+++ lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab2
+//
+//  Created by dmytro on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
